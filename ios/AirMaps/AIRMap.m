ea36e6a5f30a079de729c81e96ac802ea6ada886
Alex Khlebnikov <a.khlebnikov@2gis.ru>
2017-02-03 15:32:28 +0600

    add fix for apple map touch

--- ios/AirMaps/AIRMap.m
+++ ios/AirMaps/AIRMap.m
@@ -146,7 +146,17 @@ const CGFloat AIRMapZoomBoundBuffer = 0.01;
     UIView *calloutMaybe = [self.calloutView hitTest:[self.calloutView convertPoint:point fromView:self] withEvent:event];
     if (calloutMaybe) return calloutMaybe;
 
-    return [super hitTest:point withEvent:event];
+      // If it's not a callout, then always return the MKNewAnnotationContainerView which will handle pinch & zoom properly
+      // - MKMapView
+      // - - UIView
+      // - - - MKBasicMapView
+      // - - - - _MKMapLayerHostingView
+      // - - - MKScrollContainerView
+      // - - - - MKOverlayContainerView
+      // - - - MKNewAnnotationContainerView
+      // - - MKAttributionLabel
+      // см. https://github.com/airbnb/react-native-maps/pull/623/files
+      return ((UIView *)[((UIView *)[self.subviews objectAtIndex:0]).subviews objectAtIndex:2]);
 }
 
 #pragma mark SMCalloutViewDelegate
