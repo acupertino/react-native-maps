230a9036b05c9660ea7b4e8f73abc74522eec6a3
Alex Khlebnikov <a.khlebnikov@2gis.ru>
2017-02-03 18:56:56 +0600

    de/select annotation via map

--- ios/AirMaps/AIRMapMarkerManager.m
+++ ios/AirMaps/AIRMapMarkerManager.m
@@ -61,7 +61,8 @@ RCT_EXPORT_METHOD(showCallout:(nonnull NSNumber *)reactTag)
         if (![view isKindOfClass:[AIRMapMarker class]]) {
             RCTLogError(@"Invalid view returned from registry, expecting AIRMap, got: %@", view);
         } else {
-            [(AIRMapMarker *) view showCalloutView];
+            AIRMapMarker *marker = (AIRMapMarker *)view;
+            [marker.map selectAnnotation:marker animated:NO];
         }
     }];
 }
@@ -73,7 +74,8 @@ RCT_EXPORT_METHOD(hideCallout:(nonnull NSNumber *)reactTag)
         if (![view isKindOfClass:[AIRMapMarker class]]) {
             RCTLogError(@"Invalid view returned from registry, expecting AIRMap, got: %@", view);
         } else {
-            [(AIRMapMarker *) view hideCalloutView];
+            AIRMapMarker *marker = (AIRMapMarker *)view;
+            [marker.map deselectAnnotation:marker animated:NO];
         }
     }];
 }
